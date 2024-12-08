6ba88713f1b30bddbbacf5d8ff56ae2ec41a2a92
Steve Dunham <dunhamsteve@gmail.com>
2024-12-07 19:08:14 -0800

    fix CI

--- playground/src/worker.ts
+++ playground/src/worker.ts
@@ -2,7 +2,7 @@ import { shim } from "./emul";
 import { archive, preload } from "./preload";
 import { CompileReq, CompileRes } from "./types";
 
-const handleMessage = async function (ev: {data: CompileReq}) {
+const handleMessage = async function (ev: { data: CompileReq }) {
   console.log("message", ev.data);
   await preload;
   shim.archive = archive;
@@ -27,13 +27,13 @@ const handleMessage = async function (ev: {data: CompileReq}) {
     shim.stdout += "\n" + String(e);
   }
   let duration = +new Date() - start;
-  console.log(`shim.process ${fn} in ${duration} ms`);
+  console.log(`process ${fn} in ${duration} ms`);
   let javascript = new TextDecoder().decode(shim.files[outfile]);
   let output = shim.stdout;
   sendResponse({ javascript, output, duration });
 };
 
 // hooks for worker.html to override
-let sendResponse : (_: CompileRes) => void = postMessage;
+let sendResponse: (_: CompileRes) => void = postMessage;
 onmessage = handleMessage;
 importScripts("newt.js");
